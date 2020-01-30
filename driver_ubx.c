e9f75f78fa86f3b41ee3f8bfebdd0c228858da7b
cabrol <quentin@aeler.com>
2020-01-29 18:14:55 -0800

    driver_ubx.c: Add stub for UBX-LOG-BATCH decode.
    Signed-off-by: Gary E. Miller <gem@rellim.com>

--- driver_ubx.c
+++ driver_ubx.c
@@ -64,6 +64,8 @@
 
 static gps_mask_t ubx_parse(struct gps_device_t *session, unsigned char *buf,
                             size_t len);
+static gps_mask_t ubx_msg_log_batch(struct gps_device_t *session,
+                                    unsigned char *buf, size_t data_len);
 static gps_mask_t ubx_msg_nav_eoe(struct gps_device_t *session,
                                   unsigned char *buf, size_t data_len);
 static gps_mask_t ubx_msg_nav_dop(struct gps_device_t *session,
@@ -314,6 +316,26 @@ ubx_msg_mon_ver(struct gps_device_t *session, unsigned char *buf,
     GPSD_LOG(LOG_INF, &session->context->errout,
              "UBX-MON-VER: %s %s\n",
              session->subtype, session->subtype1);
+}
+
+/**
+ * UBX-LOG-BATCH entry only part of UBX protocol
+ * Used for GPS standalone operation (internal batch retrieval)
+ */
+static gps_mask_t
+ubx_msg_log_batch(struct gps_device_t *session, unsigned char *buf,
+                  size_t data_len)
+{
+    gps_mask_t mask = 0;
+
+    /* u-blox 8 100 bytes payload */
+    if (100 > data_len) {
+        GPSD_LOG(LOG_WARN, &session->context->errout,
+                 "UBX-LOG-BATCH: runt len %zd", data_len);
+        return 0;
+    }
+
+    return mask;
 }
 
 /*
@@ -1770,6 +1792,7 @@ gps_mask_t ubx_parse(struct gps_device_t * session, unsigned char *buf,
 
     case UBX_LOG_BATCH:
         GPSD_LOG(LOG_PROG, &session->context->errout, "UBX-LOG-BATCH\n");
+        mask = ubx_msg_log_batch(session, &buf[UBX_PREFIX_LEN], data_len);
         break;
 
     case UBX_MON_BATCH:
