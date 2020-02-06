d5d9ce05623d5905d01a807f36c5faca7db986fe
Gary E. Miller <gem@rellim.com>
2020-02-06 14:41:03 -0800

    driver_ubx.c: Fix some LOG-BATCH scaling issues.

--- driver_ubx.c
+++ driver_ubx.c
@@ -394,17 +394,17 @@ ubx_msg_log_batch(struct gps_device_t *session, unsigned char *buf UNUSED,
 
     if (1 == (contentValid & 1)) {
         //  iTOW = getleu32(buf, 4);
-        session->gpsdata.log.tAcc = 1.0e-2 * getleu32(buf, 16);
+        session->gpsdata.log.tAcc = (double)getleu32(buf, 16);
         session->gpsdata.log.numSV = getub(buf, 27);
         session->gpsdata.log.altMSL = 1.0e-3 * getles32(buf, 40);
-        session->gpsdata.log.hAcc = 1.0e-2 * getleu32(buf, 44);
+        session->gpsdata.log.hAcc = 1.0e-3 * getleu32(buf, 44);
         session->gpsdata.log.vAcc = 1.0e-3 * getleu32(buf, 48);
         session->gpsdata.log.velN = 1.0e-3 * getles32(buf, 52);
         session->gpsdata.log.velE = 1.0e-3 * getles32(buf, 56);
         session->gpsdata.log.velD = 1.0e-3 * getles32(buf, 60);
         session->gpsdata.log.sAcc = 1.0e-3 * getleu32(buf, 72);
         session->gpsdata.log.headAcc = 1.0e-5 * getleu32(buf, 76);
-        session->gpsdata.log.pDOP = 1.0e-5 * getleu32(buf, 80);
+        session->gpsdata.log.pDOP = 1.0e-2 * getleu32(buf, 80);
         GPSD_LOG(LOG_INF, &session->context->errout,
                 "UBX-LOG-BATCH extraPVT: time=%s index_cnt=%d"
                  " tAcc=%.2f numSV=%d altMSL=%.3f hAcc=%.2f vAcc=%.3f"
