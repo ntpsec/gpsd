92defd5041cb7b470ccd573a9403d3e6fd6cbb73
Gary E. Miller <gem@rellim.com>
2020-02-06 12:52:16 -0800

    gpsd_json.c: Add velNED to class LOG.
    But no velNED in the regressions to show the change...

--- gpsd_json.c
+++ gpsd_json.c
@@ -149,6 +149,19 @@ static void json_log_dump(const struct gps_device_t *session,
     if (0 != isfinite(logp->heading)) {
         str_appendf(reply, replylen, ",\"heading\":%.0f", logp->heading);
     }
+    if (0 != isfinite(logp->velN) &&
+        0 != isfinite(logp->velE)) {
+        // 2D fix needs velN and velE
+        str_appendf(reply, replylen,
+                    "\"velN\":%.3f,\"velE\":%.3f,",
+                    logp->velN,
+                    logp->velE);
+        if (0 != isfinite(logp->velD)) {
+            // 3D fix add velD
+            str_appendf(reply, replylen, "\"velD\":%.3f,",
+                        logp->velD);
+        }
+    }
     if (0 != isfinite(logp->distance)) {
         str_appendf(reply, replylen, ",\"distance\":%.0f", logp->distance);
     }
