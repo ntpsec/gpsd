ntpsec/gpsd

commit 100b402ac421ba942bc09b91c37bae573abe9778

SConstruct: Remove old packet.so if present.
Python (at least under some circumstances) prefers the old-style
packet.so to the new FFI-based version, leading to confusion.
The old packet.so needs to be removed to avoid this, both from
the build tree and from the install location.

TESTED:
Ran both build and install with old packet.so files present.
Verified that they were removed.

SConstruct
@@ -1676,6 +1676,11 @@ packet_ffi_shared = env.SharedLibrary(target="gpsdpacket",
 env.Clean(libgps_shared, "gps_maskdump.c")
 libraries = [libgps_shared, packet_ffi_shared]
 
+# Make sure the old-style packet.so is gone, since it may be preferred
+old_packet_so = 'packet%s' % python_config['SO']
+del_old_so = Command('del-old-so', '', Delete('gps/%s' % old_packet_so))
+env.Depends(packet_ffi_shared, del_old_so)
+
 # Only attempt to create the qt library if we have shared turned on
 # otherwise we have a mismash of objects in library
 if qt_env:
@@ -2138,12 +2143,18 @@ if env['python'] and not cleaning and not helping:
     python_modules_install = python_env.Install(DESTDIR + python_module_dir,
                                                 python_modules)
 
+    python_oldso_remove = python_env.Command('uninstall-old-so', '',
+                                             Delete(DESTDIR +
+                                             python_module_dir + os.sep +
+                                             old_packet_so))
+
     python_progs_install = python_env.Install(installdir('bindir'),
                                               python_progs)
 
     python_egg_info_install = python_env.Install(DESTDIR + str(python_libdir),
                                                  python_egg_info)
     python_install = [python_modules_install,
+                      python_oldso_remove,
                       python_progs_install,
                       python_egg_info_install,
                       # We don't need the directory explicitly for the

commit b400a4a2cf7285abb05041d2cdaf1d5b0ec49a09

SConstruct: Fix clean/help bug introduced by 100b402ac.
The dummy python_config created in the "cleaning or helping"
case didn't work for the packet.so removal code.

TESTED:
scons -h and -c now work.

SConstruct
@@ -1379,7 +1379,7 @@ if cleaning or helping:
     py_config_text = str(eval(PYTHON_CONFIG_CALL))
     python_libdir = str(eval(PYTHON_LIBDIR_CALL))
     config.env['xgps_deps'] = False
-    python_config = []
+    python_config = {}
 
 elif config.env['python']:
     target_python_path = None
@@ -1679,7 +1679,7 @@ env.Clean(libgps_shared, "gps_maskdump.c")
 libraries = [libgps_shared, packet_ffi_shared]
 
 # Make sure the old-style packet.so is gone, since it may be preferred
-old_packet_so = 'packet%s' % python_config['SO']
+old_packet_so = 'packet%s' % python_config.get('SO', '.so')
 del_old_so = Command('del-old-so', '', Delete('gps/%s' % old_packet_so))
 env.Depends(packet_ffi_shared, del_old_so)
 