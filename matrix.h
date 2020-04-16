bf20ac53a424ca6c59bffefbd591669ad4ca2172
Gary E. Miller <gem@rellim.com>
2020-04-16 14:35:36 -0700

    matrix.h: Fix copyright, remove tabs.

--- matrix.h
+++ matrix.h
@@ -1,7 +1,7 @@
 /*
  * matrix.h - matrix-algebra prototypes
  *
- * This file is Copyright (c)2010 by the GPSD project
+ * This file is Copyright 2010 by the GPSD project
  * SPDX-License-Identifier: BSD-2-clause
  */
 
@@ -9,3 +9,4 @@ extern bool matrix_invert(double mat[4][4], double inverse[4][4]);
 extern void matrix_symmetrize(double mat[4][4], double inverse[4][4]);
 
 /* end */
+// vim: set expandtab shiftwidth=4
