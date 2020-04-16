13ae86585b78919b54ede353fda62b00016a83f4
Gary E. Miller <gem@rellim.com>
2020-04-16 15:29:19 -0700

    shared_json.c: Fix copyright, remove tabs.

--- shared_json.c
+++ shared_json.c
@@ -10,7 +10,7 @@ the daemon and the client library.
 
 PERMISSIONS
   Written by Eric S. Raymond, 2009
-  This file is Copyright (c) 2009-2018 The GPSD project
+  This file is Copyright 2009 The GPSD project
   SPDX-License-Identifier: BSD-2-clause
 
 ***************************************************************************/
@@ -26,95 +26,95 @@ PERMISSIONS
 #include "timespec.h"
 
 int json_device_read(const char *buf,
-		     struct devconfig_t *dev,
-		     const char **endptr)
+                     struct devconfig_t *dev,
+                     const char **endptr)
 {
     // initialized to shut up clang
     double d_cycle = 0.0, d_mincycle = 0.0;
 
     /* *INDENT-OFF* */
     const struct json_attr_t json_attrs_device[] = {
-	{"class",      t_check,      .dflt.check = "DEVICE"},
+        {"class",      t_check,      .dflt.check = "DEVICE"},
 
         {"path",       t_string,     .addr.string  = dev->path,
-	                                .len = sizeof(dev->path)},
+                                        .len = sizeof(dev->path)},
         // odd, device->gpsdata.online is sent, but put in dev->activated?
-	{"activated",  t_time,       .addr.ts = &dev->activated,
-				        .dflt.ts = {0, 0}},
-	{"flags",      t_integer,    .addr.integer = &dev->flags},
-	{"driver",     t_string,     .addr.string  = dev->driver,
-	                                .len = sizeof(dev->driver)},
-	{"subtype",    t_string,     .addr.string  = dev->subtype,
-	                                .len = sizeof(dev->subtype)},
-	{"subtype1",   t_string,     .addr.string  = dev->subtype1,
-	                                .len = sizeof(dev->subtype1)},
+        {"activated",  t_time,       .addr.ts = &dev->activated,
+                                        .dflt.ts = {0, 0}},
+        {"flags",      t_integer,    .addr.integer = &dev->flags},
+        {"driver",     t_string,     .addr.string  = dev->driver,
+                                        .len = sizeof(dev->driver)},
+        {"subtype",    t_string,     .addr.string  = dev->subtype,
+                                        .len = sizeof(dev->subtype)},
+        {"subtype1",   t_string,     .addr.string  = dev->subtype1,
+                                        .len = sizeof(dev->subtype1)},
         {"hexdata",    t_string,     .addr.string  = dev->hexdata,
-	                                .len = sizeof(dev->hexdata)},
-	{"native",     t_integer,    .addr.integer = &dev->driver_mode,
-				        .dflt.integer = DEVDEFAULT_NATIVE},
-	{"bps",	       t_uinteger,   .addr.uinteger = &dev->baudrate,
-				        .dflt.uinteger = DEVDEFAULT_BPS},
-	{"parity",     t_character,  .addr.character = &dev->parity,
+                                        .len = sizeof(dev->hexdata)},
+        {"native",     t_integer,    .addr.integer = &dev->driver_mode,
+                                        .dflt.integer = DEVDEFAULT_NATIVE},
+        {"bps",        t_uinteger,   .addr.uinteger = &dev->baudrate,
+                                        .dflt.uinteger = DEVDEFAULT_BPS},
+        {"parity",     t_character,  .addr.character = &dev->parity,
                                         .dflt.character = DEVDEFAULT_PARITY},
-	{"stopbits",   t_uinteger,   .addr.uinteger = &dev->stopbits,
-				        .dflt.uinteger = DEVDEFAULT_STOPBITS},
-	{"cycle",      t_real,       .addr.real = &d_cycle,
-				        .dflt.real = NAN},
-	{"mincycle",   t_real,       .addr.real = &d_mincycle,
-				        .dflt.real = NAN},
-	{NULL},
+        {"stopbits",   t_uinteger,   .addr.uinteger = &dev->stopbits,
+                                        .dflt.uinteger = DEVDEFAULT_STOPBITS},
+        {"cycle",      t_real,       .addr.real = &d_cycle,
+                                        .dflt.real = NAN},
+        {"mincycle",   t_real,       .addr.real = &d_mincycle,
+                                        .dflt.real = NAN},
+        {NULL},
     };
     /* *INDENT-ON* */
     int status;
 
     status = json_read_object(buf, json_attrs_device, endptr);
     if (status != 0)
-	return status;
+        return status;
 
     if (0 == isfinite(d_cycle)) {
-	dev->cycle.tv_sec = 0;
-	dev->cycle.tv_nsec = 0;
+        dev->cycle.tv_sec = 0;
+        dev->cycle.tv_nsec = 0;
     } else {
-	DTOTS(&dev->cycle, d_cycle);
+        DTOTS(&dev->cycle, d_cycle);
     }
     if (0 == isfinite(d_mincycle)) {
-	dev->mincycle.tv_sec = 0;
-	dev->mincycle.tv_nsec = 0;
+        dev->mincycle.tv_sec = 0;
+        dev->mincycle.tv_nsec = 0;
     } else {
-	DTOTS(&dev->cycle, d_mincycle);
+        DTOTS(&dev->cycle, d_mincycle);
     }
 
     return 0;
 }
 
 int json_watch_read(const char *buf,
-		    struct gps_policy_t *ccp,
-		    const char **endptr)
+                    struct gps_policy_t *ccp,
+                    const char **endptr)
 {
     bool dummy_pps_flag;
     /* *INDENT-OFF* */
     struct json_attr_t chanconfig_attrs[] = {
-	{"class",          t_check,    .dflt.check = "WATCH"},
+        {"class",          t_check,    .dflt.check = "WATCH"},
 
-	{"enable",         t_boolean,  .addr.boolean = &ccp->watcher,
+        {"enable",         t_boolean,  .addr.boolean = &ccp->watcher,
                                           .dflt.boolean = true},
-	{"json",           t_boolean,  .addr.boolean = &ccp->json,
+        {"json",           t_boolean,  .addr.boolean = &ccp->json,
                                           .nodefault = true},
-	{"raw",	           t_integer,  .addr.integer = &ccp->raw,
-	                                  .nodefault = true},
-	{"nmea",	   t_boolean,  .addr.boolean = &ccp->nmea,
-	                                  .nodefault = true},
-	{"scaled",         t_boolean,  .addr.boolean = &ccp->scaled},
-	{"timing",         t_boolean,  .addr.boolean = &ccp->timing},
-	{"split24",        t_boolean,  .addr.boolean = &ccp->split24},
-	{"pps",            t_boolean,  .addr.boolean = &ccp->pps},
-	{"device",         t_string,   .addr.string = ccp->devpath,
-	                                  .len = sizeof(ccp->devpath)},
-	{"remote",         t_string,   .addr.string = ccp->remote,
-	                                  .len = sizeof(ccp->remote)},
-	{"pps",            t_boolean,  .addr.boolean = &dummy_pps_flag,
+        {"raw",            t_integer,  .addr.integer = &ccp->raw,
+                                          .nodefault = true},
+        {"nmea",           t_boolean,  .addr.boolean = &ccp->nmea,
+                                          .nodefault = true},
+        {"scaled",         t_boolean,  .addr.boolean = &ccp->scaled},
+        {"timing",         t_boolean,  .addr.boolean = &ccp->timing},
+        {"split24",        t_boolean,  .addr.boolean = &ccp->split24},
+        {"pps",            t_boolean,  .addr.boolean = &ccp->pps},
+        {"device",         t_string,   .addr.string = ccp->devpath,
+                                          .len = sizeof(ccp->devpath)},
+        {"remote",         t_string,   .addr.string = ccp->remote,
+                                          .len = sizeof(ccp->remote)},
+        {"pps",            t_boolean,  .addr.boolean = &dummy_pps_flag,
                                           .nodefault = false},
-	{NULL},
+        {NULL},
     };
     /* *INDENT-ON* */
     int status;
@@ -126,3 +126,4 @@ int json_watch_read(const char *buf,
 #endif /* SOCKET_EXPORT_ENABLE */
 
 /* shared_json.c ends here */
+// vim: set expandtab shiftwidth=4
