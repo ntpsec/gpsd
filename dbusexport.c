81ecc04f7190a4bf431661a430b0ebff77237e61
Gary E. Miller <gem@rellim.com>
2020-04-16 18:04:08 -0700

    dbusexport.c: Fix copyright, remove tabs.

--- dbusexport.c
+++ dbusexport.c
@@ -1,5 +1,5 @@
 /*
- * This file is Copyright (c) 2010-2019 by the GPSD project
+ * This file is Copyright 2010 by the GPSD project
  * SPDX-License-Identifier: BSD-2-clause
  */
 #include "gpsd_config.h"  /* must be before all includes */
@@ -22,26 +22,26 @@ int initialize_dbus_connection(void)
     dbus_error_init(&error);
     connection = dbus_bus_get(DBUS_BUS_SYSTEM, &error);
     if (connection == NULL) {
-	dbus_error_free(&error);
-	/* report error */
-	return 1;
+        dbus_error_free(&error);
+        /* report error */
+        return 1;
     }
     return 0;
 }
 
+/* sends the current fix data for this channel via dbus */
 void send_dbus_fix(struct gps_device_t *channel)
 {
-/* sends the current fix data for this channel via dbus */
     struct gps_data_t *gpsdata;
     struct gps_fix_t *gpsfix;
     DBusMessage *message;
     /*DBusMessageIter   iter; */
-    dbus_uint32_t serial;	/* collected, but not used */
+    dbus_uint32_t serial;       /* collected, but not used */
     char *gpsd_devname;
 
     /* if the connection is non existent, return without doing anything */
     if (connection == NULL)
-	return;
+        return;
 
     gpsdata = &(channel->gpsdata);
     gpsfix = &(gpsdata->fix);
@@ -52,25 +52,26 @@ void send_dbus_fix(struct gps_device_t *channel)
      * assume altMSL */
     message = dbus_message_new_signal("/org/gpsd", "org.gpsd", "fix");
     dbus_message_append_args(message,
-			     DBUS_TYPE_DOUBLE, &(gpsfix->time),
-			     DBUS_TYPE_INT32, &(gpsfix->mode),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->ept),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->latitude),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->longitude),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->eph),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->altMSL),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->epv),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->track),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->epd),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->speed),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->eps),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->climb),
-			     DBUS_TYPE_DOUBLE, &(gpsfix->epc),
-			     DBUS_TYPE_STRING, &gpsd_devname,
-			     DBUS_TYPE_INVALID);
+                             DBUS_TYPE_DOUBLE, &(gpsfix->time),
+                             DBUS_TYPE_INT32, &(gpsfix->mode),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->ept),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->latitude),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->longitude),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->eph),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->altMSL),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->epv),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->track),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->epd),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->speed),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->eps),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->climb),
+                             DBUS_TYPE_DOUBLE, &(gpsfix->epc),
+                             DBUS_TYPE_STRING, &gpsd_devname,
+                             DBUS_TYPE_INVALID);
     dbus_message_set_no_reply(message, TRUE);
     dbus_connection_send(connection, message, &serial);
     dbus_message_unref(message);
 }
 
 #endif /* defined(DBUS_EXPORT_ENABLE) */
+// vim: set expandtab shiftwidth=4
