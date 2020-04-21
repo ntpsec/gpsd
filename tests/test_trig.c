78863ff28c84312ad97ed87c9f4b1bbd69735a10
Gary E. Miller <gem@rellim.com>
2020-04-20 19:01:38 -0700

    test_trig.c: Fix copyright, remove tabs.

--- tests/test_trig.c
+++ tests/test_trig.c
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2006 Chris Kuethe <chris.kuethe@gmail.com>
- * Copyright (c) 2009 BBN Technologies (Greg Troxel)
+ * Copyright 2006 Chris Kuethe <chris.kuethe@gmail.com>
+ * Copyright 2009 BBN Technologies (Greg Troxel)
  *
- * This file is Copyright (c)2005-2019 by the GPSD project
+ * This file is Copyright 2005 by the GPSD project
  * SPDX-License-Identifier: BSD-2-clause
  */
 
@@ -16,31 +16,32 @@
 int test_trig(void);
 
 int main(void) {
-	test_trig();
+        test_trig();
 
-	/* For now, no evaluation. */
-	return 0;
+        /* For now, no evaluation. */
+        return 0;
 }
 
 #define Deg2Rad(x) ((x) * (2 * M_PI / 360.0))
 
 int test_trig(void) {
-	int i;
-	double arg;
-	double res;
+        int i;
+        double arg;
+        double res;
 
-	for (i = 0; i <= 360; i++) {
-		arg = Deg2Rad(i);
-		res = sin(arg);
-		printf("sin(%.30f) = %.30f\n", arg, res);
-	}
+        for (i = 0; i <= 360; i++) {
+                arg = Deg2Rad(i);
+                res = sin(arg);
+                printf("sin(%.30f) = %.30f\n", arg, res);
+        }
 
-	for (i = 0; i <= 360; i++) {
-		arg = Deg2Rad(i);
-		res = cos(arg);
-		printf("cos(%.30f) = %.30f\n", arg, res);
-	}
+        for (i = 0; i <= 360; i++) {
+                arg = Deg2Rad(i);
+                res = cos(arg);
+                printf("cos(%.30f) = %.30f\n", arg, res);
+        }
 
-	/* Always claim success. */
-	return 0;
+        /* Always claim success. */
+        return 0;
 }
+// vim: set expandtab shiftwidth=4
