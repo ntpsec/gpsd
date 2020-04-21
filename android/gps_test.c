5fe6d3ff39b71d7167757ffc4818f1961f2b9e85
Gary E. Miller <gem@rellim.com>
2020-04-20 19:22:33 -0700

    android/gps_test.c: Fix copyright, remove tabs.

--- android/gps_test.c
+++ android/gps_test.c
@@ -1,14 +1,14 @@
 /*
- * This file is Copyright (c) 2019 by the GPSD project
+ * This file is Copyright 2019 by the GPSD project
  * SPDX-License-Identifier: BSD-2-clause
  */
 
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
 
 #include "gps.h"
 
@@ -26,47 +26,47 @@ int main (int argc, char **argv) {
     memset(&gps_data, 0, sizeof(gps_data));
 
     while (1) {
-	if (gpsopen < 0) {
-	    gpsopen = gps_open((argc == 1 ? "localhost" : argv[1]),
+        if (gpsopen < 0) {
+            gpsopen = gps_open((argc == 1 ? "localhost" : argv[1]),
                                "2947", &gps_data);
-	    if (0 == gpsopen) {
-		printf("gps_open returned 0 (success)\n");
-		gps_stream(&gps_data, WATCH_ENABLE, NULL);
-	    } else {
-		printf("gps_open failed, returned: %d\n", gpsopen);
-		gpsopen = -1;
-		sleep(5);
-		continue;
-	    }
-	}
+            if (0 == gpsopen) {
+                printf("gps_open returned 0 (success)\n");
+                gps_stream(&gps_data, WATCH_ENABLE, NULL);
+            } else {
+                printf("gps_open failed, returned: %d\n", gpsopen);
+                gpsopen = -1;
+                sleep(5);
+                continue;
+            }
+        }
 
-	if (gps_waiting (&gps_data, 2000000)) {
-	    errno = 0;
-	    if (gps_read (&gps_data, NULL, 0) != -1) {
-		if (gps_data.status >= 1 && gps_data.fix.mode >= 2){
-		    printf("\nHave a fix: ");
-		    if (gps_data.fix.mode == 2)
+        if (gps_waiting (&gps_data, 2000000)) {
+            errno = 0;
+            if (gps_read (&gps_data, NULL, 0) != -1) {
+                if (gps_data.status >= 1 && gps_data.fix.mode >= 2){
+                    printf("\nHave a fix: ");
+                    if (gps_data.fix.mode == 2)
                         printf("2D\n");
                     else
                         printf("3D\n");
 
-		    printf("Latitude: %f\n", gps_data.fix.latitude);
-		    printf("Longitude: %f\n", gps_data.fix.longitude);
-		    printf("Speed: %f\n", gps_data.fix.speed);
-		    printf("Bearing: %f\n", gps_data.fix.track);
-		    printf("H Accuracy: %f\n", gps_data.fix.eph);
-		    printf("S Accuracy: %f\n", gps_data.fix.eps);
-		    printf("B Accuracy: %f\n", gps_data.fix.epd);
-		    printf("Time: %ld\n", (long) gps_data.fix.time);
-		    printf("Altitude: %f\n", gps_data.fix.altitude);
-		    printf("V Accuracy: %f\n\n", gps_data.fix.epv);
-		}
+                    printf("Latitude: %f\n", gps_data.fix.latitude);
+                    printf("Longitude: %f\n", gps_data.fix.longitude);
+                    printf("Speed: %f\n", gps_data.fix.speed);
+                    printf("Bearing: %f\n", gps_data.fix.track);
+                    printf("H Accuracy: %f\n", gps_data.fix.eph);
+                    printf("S Accuracy: %f\n", gps_data.fix.eps);
+                    printf("B Accuracy: %f\n", gps_data.fix.epd);
+                    printf("Time: %ld\n", (long) gps_data.fix.time);
+                    printf("Altitude: %f\n", gps_data.fix.altitude);
+                    printf("V Accuracy: %f\n\n", gps_data.fix.epv);
+                }
 
-		printf("Satellites visible: %d\n",
+                printf("Satellites visible: %d\n",
                        gps_data.satellites_visible);
-		for (int i = 0; i < gps_data.satellites_visible; i++) {
-		    printf("SV type: ");
-		    switch (gps_data.skyview[i].gnssid) {
+                for (int i = 0; i < gps_data.satellites_visible; i++) {
+                    printf("SV type: ");
+                    switch (gps_data.skyview[i].gnssid) {
                     case 0:
                             printf("GPS, ");
                             break;
@@ -88,17 +88,18 @@ int main (int argc, char **argv) {
                     case 6:
                             printf("Glonass, ");
                             break;
-		    }
+                    }
 
-		    printf("SVID: %d, SNR: %d, Elevation: %d, "
+                    printf("SVID: %d, SNR: %d, Elevation: %d, "
                            "Azimuth: %d, Used: %d\n",
                             gps_data.skyview[i].svid,
                             (int)gps_data.skyview[i].ss,
                             gps_data.skyview[i].elevation,
                             gps_data.skyview[i].azimuth,
                             gps_data.skyview[i].used);
-		}
-	    }
-	}
+                }
+            }
+        }
     }
 }
+// vim: set expandtab shiftwidth=4
