5c1ff2fcbeffd8fedd77db1f300bd6f09dc405a3
Gary E. Miller <gem@rellim.com>
2020-05-01 18:23:20 -0700

    libgpsmm.cpp: Fix copyright, remove tabs.

--- libgpsmm.cpp
+++ libgpsmm.cpp
@@ -1,6 +1,7 @@
 /*
- * Copyright (C) 2005 Alfredo Pironti
+ * Copyright 2005 Alfredo Pironti
  *
+ * This file is Copyright 2005 by the GPSD project
  * SPDX-License-Identifier: BSD-2-clause
  */
 
@@ -13,47 +14,47 @@ struct gps_data_t* gpsmm::gps_inner_open(const char *host, const char *port)
 {
     const bool err = (gps_open(host, port, gps_state()) != 0);
     if ( err ) {
-	to_user = NULL;
-	return NULL;
+        to_user = NULL;
+        return NULL;
     }
     else { // connection successfully opened
-	to_user= new struct gps_data_t;
-	return backup(); //we return the backup of our internal structure
+        to_user= new struct gps_data_t;
+        return backup(); //we return the backup of our internal structure
     }
 }
 
 struct gps_data_t* gpsmm::stream(int flags)
 {
     if (to_user == NULL)
-	return NULL;
+        return NULL;
     else if (gps_stream(gps_state(),flags, NULL)==-1) {
-	return NULL;
+        return NULL;
     }
     else {
-	return backup();
+        return backup();
     }
 }
 
 struct gps_data_t* gpsmm::send(const char *request)
 {
     if (gps_send(gps_state(),request)==-1) {
-	return NULL;
+        return NULL;
     }
     else {
-	return backup();
+        return backup();
     }
 }
 
 struct gps_data_t* gpsmm::read(void)
 {
     if (gps_read(gps_state(), NULL, 0)<=0) {
-	// we return null if there was a read() error, if no
-	// data was ready in POLL_NOBLOCK mode, or if the
-	// connection is closed by gpsd
-	return NULL;
+        // we return null if there was a read() error, if no
+        // data was ready in POLL_NOBLOCK mode, or if the
+        // connection is closed by gpsd
+        return NULL;
     }
     else {
-	return backup();
+        return backup();
     }
 }
 
@@ -82,13 +83,14 @@ void gpsmm::enable_debug(int level, FILE *fp)
 // cppcheck-suppress unusedFunction
 bool gpsmm::is_open(void)
 {
-	return to_user != NULL;
+        return to_user != NULL;
 }
 
 gpsmm::~gpsmm()
 {
     if ( to_user != NULL ) {
-	(void)gps_close(gps_state());
-	delete to_user;
+        (void)gps_close(gps_state());
+        delete to_user;
     }
 }
+// vim: set expandtab shiftwidth=4
