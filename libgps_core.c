4f0ce7f8527aa3bddc53485ed6783e92d9f1b365
Gary E. Miller <gem@rellim.com>
2020-05-01 18:09:32 -0700

    libgps_core.c: remove tabs.

--- libgps_core.c
+++ libgps_core.c
@@ -38,15 +38,15 @@ void gps_enable_debug(int level, FILE * fp)
 void libgps_trace(int errlevel, const char *fmt, ...)
 {
     if (errlevel <= libgps_debuglevel) {
-	char buf[BUFSIZ];
-	va_list ap;
+        char buf[BUFSIZ];
+        va_list ap;
 
-	(void)strlcpy(buf, "libgps: ", sizeof(buf));
-	va_start(ap, fmt);
-	str_vappendf(buf, sizeof(buf), fmt, ap);
-	va_end(ap);
+        (void)strlcpy(buf, "libgps: ", sizeof(buf));
+        va_start(ap, fmt);
+        str_vappendf(buf, sizeof(buf), fmt, ap);
+        va_end(ap);
 
-	(void)fputs(buf, debugfp);
+        (void)fputs(buf, debugfp);
     }
 }
 
@@ -57,30 +57,30 @@ void libgps_trace(int errlevel, const char *fmt, ...)
 #endif /* SOCKET_EXPORT_ENABLE */
 
 int gps_open(const char *host,
-	     const char *port CONDITIONALLY_UNUSED,
-	     struct gps_data_t *gpsdata)
+             const char *port CONDITIONALLY_UNUSED,
+             struct gps_data_t *gpsdata)
 {
     int status = -1;
 
     if (!gpsdata)
-	return -1;
+        return -1;
 
 #ifdef SHM_EXPORT_ENABLE
     if (host != NULL && strcmp(host, GPSD_SHARED_MEMORY) == 0) {
-	status = gps_shm_open(gpsdata);
-	if (status == -1)
-	    status = SHM_NOSHARED;
-	else if (status == -2)
-	    status = SHM_NOATTACH;
+        status = gps_shm_open(gpsdata);
+        if (status == -1)
+            status = SHM_NOSHARED;
+        else if (status == -2)
+            status = SHM_NOATTACH;
     }
 #define USES_HOST
 #endif /* SHM_EXPORT_ENABLE */
 
 #ifdef DBUS_EXPORT_ENABLE
     if (host != NULL && strcmp(host, GPSD_DBUS_EXPORT) == 0) {
-	status = gps_dbus_open(gpsdata);
-	if (status != 0)
-	    status = DBUS_FAILURE;
+        status = gps_dbus_open(gpsdata);
+        if (status != 0)
+            status = DBUS_FAILURE;
     }
 #define USES_HOST
 #endif /* DBUS_EXPORT_ENABLE */
@@ -118,8 +118,8 @@ int gps_close(struct gps_data_t *gpsdata CONDITIONALLY_UNUSED)
 
 #ifdef SHM_EXPORT_ENABLE
     if (BAD_SOCKET((intptr_t)(gpsdata->gps_fd))) {
-	gps_shm_close(gpsdata);
-	status = 0;
+        gps_shm_close(gpsdata);
+        status = 0;
     }
 #endif /* SHM_EXPORT_ENABLE */
 
@@ -129,7 +129,7 @@ int gps_close(struct gps_data_t *gpsdata CONDITIONALLY_UNUSED)
     }
 #endif /* SOCKET_EXPORT_ENABLE */
 
-	return status;
+        return status;
 }
 
 /* read from a gpsd connection
@@ -153,7 +153,7 @@ int gps_read(struct gps_data_t *gpsdata CONDITIONALLY_UNUSED,
 
 #ifdef SHM_EXPORT_ENABLE
     if (BAD_SOCKET((intptr_t)(gpsdata->gps_fd))) {
-	status = gps_shm_read(gpsdata);
+        status = gps_shm_read(gpsdata);
     }
 #endif /* SHM_EXPORT_ENABLE */
 
@@ -164,7 +164,7 @@ int gps_read(struct gps_data_t *gpsdata CONDITIONALLY_UNUSED,
 #endif /* SOCKET_EXPORT_ENABLE */
 
     libgps_debug_trace((DEBUG_CALLS, "gps_read() -> %d (%s)\n",
-			status, gps_maskdump(gpsdata->set)));
+                        status, gps_maskdump(gpsdata->set)));
 
     return status;
 }
@@ -181,7 +181,7 @@ int gps_send(struct gps_data_t *gpsdata CONDITIONALLY_UNUSED,
     (void)vsnprintf(buf, sizeof(buf) - 2, fmt, ap);
     va_end(ap);
     if (buf[strlen(buf) - 1] != '\n')
-	(void)strlcat(buf, "\n", sizeof(buf));
+        (void)strlcat(buf, "\n", sizeof(buf));
 
 #ifdef SOCKET_EXPORT_ENABLE
     status = gps_sock_send(gpsdata, buf);
@@ -191,8 +191,8 @@ int gps_send(struct gps_data_t *gpsdata CONDITIONALLY_UNUSED,
 }
 
 int gps_stream(struct gps_data_t *gpsdata CONDITIONALLY_UNUSED,
-	unsigned int flags CONDITIONALLY_UNUSED,
-	void *d CONDITIONALLY_UNUSED)
+        unsigned int flags CONDITIONALLY_UNUSED,
+        void *d CONDITIONALLY_UNUSED)
 {
     int status = -1;
 
@@ -226,21 +226,21 @@ bool gps_waiting(const struct gps_data_t *gpsdata CONDITIONALLY_UNUSED,
 
 #ifdef SHM_EXPORT_ENABLE
     if ((intptr_t)(gpsdata->gps_fd) == SHM_PSEUDO_FD)
-	waiting = gps_shm_waiting(gpsdata, timeout);
+        waiting = gps_shm_waiting(gpsdata, timeout);
 #endif /* SHM_EXPORT_ENABLE */
 
 #ifdef SOCKET_EXPORT_ENABLE
     // cppcheck-suppress pointerPositive
     if ((intptr_t)(gpsdata->gps_fd) >= 0)
-	waiting = gps_sock_waiting(gpsdata, timeout);
+        waiting = gps_sock_waiting(gpsdata, timeout);
 #endif /* SOCKET_EXPORT_ENABLE */
 
     return waiting;
 }
 
 int gps_mainloop(struct gps_data_t *gpsdata CONDITIONALLY_UNUSED,
-		 int timeout CONDITIONALLY_UNUSED,
-		 void (*hook)(struct gps_data_t *gpsdata) CONDITIONALLY_UNUSED)
+                 int timeout CONDITIONALLY_UNUSED,
+                 void (*hook)(struct gps_data_t *gpsdata) CONDITIONALLY_UNUSED)
 {
     int status = -1;
 
@@ -248,19 +248,19 @@ int gps_mainloop(struct gps_data_t *gpsdata CONDITIONALLY_UNUSED,
 
 #ifdef SHM_EXPORT_ENABLE
     if ((intptr_t)(gpsdata->gps_fd) == SHM_PSEUDO_FD)
-	status = gps_shm_mainloop(gpsdata, timeout, hook);
+        status = gps_shm_mainloop(gpsdata, timeout, hook);
 #endif /* SHM_EXPORT_ENABLE */
 #ifdef DBUS_EXPORT_ENABLE
     if ((intptr_t)(gpsdata->gps_fd) == DBUS_PSEUDO_FD)
-	status = gps_dbus_mainloop(gpsdata, timeout, hook);
+        status = gps_dbus_mainloop(gpsdata, timeout, hook);
 #endif /* DBUS_EXPORT_ENABLE */
 #ifdef SOCKET_EXPORT_ENABLE
     if ((intptr_t)(gpsdata->gps_fd) >= 0)
-	status = gps_sock_mainloop(gpsdata, timeout, hook);
+        status = gps_sock_mainloop(gpsdata, timeout, hook);
 #endif /* SOCKET_EXPORT_ENABLE */
 
     libgps_debug_trace((DEBUG_CALLS, "gps_mainloop() -> %d (%s)\n",
-			status, gps_maskdump(gpsdata->set)));
+                        status, gps_maskdump(gpsdata->set)));
 
     return status;
 }
@@ -274,13 +274,13 @@ extern const char *gps_errstr(const int err)
 #ifndef USE_QT
 #ifdef SHM_EXPORT_ENABLE
     if (err == SHM_NOSHARED)
-	return "no shared-memory segment or daemon not running";
+        return "no shared-memory segment or daemon not running";
     else if (err == SHM_NOATTACH)
-	return "attach failed for unknown reason";
+        return "attach failed for unknown reason";
 #endif /* SHM_EXPORT_ENABLE */
 #ifdef DBUS_EXPORT_ENABLE
     if (err == DBUS_FAILURE)
-	return "DBUS initialization failure";
+        return "DBUS initialization failure";
 #endif /* DBUS_EXPORT_ENABLE */
     return netlib_errstr(err);
 #else
@@ -297,93 +297,93 @@ void libgps_dump_state(struct gps_data_t *collect)
     /* no need to dump the entire state, this is a sanity check */
 #ifndef USE_QT
     (void)fprintf(debugfp, "flags: (0x%04x) %s\n",
-		  (unsigned int)collect->set, gps_maskdump(collect->set));
+                  (unsigned int)collect->set, gps_maskdump(collect->set));
 #endif
     if (collect->set & ONLINE_SET)
-	(void)fprintf(debugfp, "ONLINE: %s\n",
+        (void)fprintf(debugfp, "ONLINE: %s\n",
                       timespec_str(&collect->online, ts_buf, sizeof(ts_buf)));
     if (collect->set & TIME_SET)
-	(void)fprintf(debugfp, "TIME: %s\n",
+        (void)fprintf(debugfp, "TIME: %s\n",
                      timespec_str(&collect->fix.time, ts_buf, sizeof(ts_buf)));
     /* NOTE: %.7f needed for cm level accurate GPS */
     if (collect->set & LATLON_SET)
-	(void)fprintf(debugfp, "LATLON: lat/lon: %.7lf %.7lf\n",
-		      collect->fix.latitude, collect->fix.longitude);
+        (void)fprintf(debugfp, "LATLON: lat/lon: %.7lf %.7lf\n",
+                      collect->fix.latitude, collect->fix.longitude);
     if (collect->set & ALTITUDE_SET)
-	(void)fprintf(debugfp, "ALTITUDE: altHAE: %lf  U: climb: %lf\n",
-		      collect->fix.altHAE, collect->fix.climb);
+        (void)fprintf(debugfp, "ALTITUDE: altHAE: %lf  U: climb: %lf\n",
+                      collect->fix.altHAE, collect->fix.climb);
     if (collect->set & SPEED_SET)
-	(void)fprintf(debugfp, "SPEED: %lf\n", collect->fix.speed);
+        (void)fprintf(debugfp, "SPEED: %lf\n", collect->fix.speed);
     if (collect->set & TRACK_SET)
-	(void)fprintf(debugfp, "TRACK: track: %lf\n", collect->fix.track);
+        (void)fprintf(debugfp, "TRACK: track: %lf\n", collect->fix.track);
     if (collect->set & MAGNETIC_TRACK_SET)
         (void)fprintf(debugfp, "MAGNETIC_TRACK: magtrack: %lf\n",
                       collect->fix.magnetic_track);
     if (collect->set & CLIMB_SET)
-	(void)fprintf(debugfp, "CLIMB: climb: %lf\n", collect->fix.climb);
+        (void)fprintf(debugfp, "CLIMB: climb: %lf\n", collect->fix.climb);
     if (collect->set & STATUS_SET) {
         // FIXME! add missing status values.  range check status!
-	const char *status_values[] = { "NO_FIX", "FIX", "DGPS_FIX" };
-	(void)fprintf(debugfp, "STATUS: status: %d (%s)\n",
-		      collect->fix.status, status_values[collect->fix.status]);
+        const char *status_values[] = { "NO_FIX", "FIX", "DGPS_FIX" };
+        (void)fprintf(debugfp, "STATUS: status: %d (%s)\n",
+                      collect->fix.status, status_values[collect->fix.status]);
     }
     if (collect->set & MODE_SET) {
-	const char *mode_values[] = { "", "NO_FIX", "MODE_2D", "MODE_3D" };
-	(void)fprintf(debugfp, "MODE: mode: %d (%s)\n",
-		      collect->fix.mode, mode_values[collect->fix.mode]);
+        const char *mode_values[] = { "", "NO_FIX", "MODE_2D", "MODE_3D" };
+        (void)fprintf(debugfp, "MODE: mode: %d (%s)\n",
+                      collect->fix.mode, mode_values[collect->fix.mode]);
     }
     if (collect->set & SATELLITE_SET)
-	(void)fprintf(debugfp,
-		      "DOP: satellites %d, pdop=%lf, hdop=%lf, vdop=%lf\n",
-		      collect->satellites_used, collect->dop.pdop,
-		      collect->dop.hdop, collect->dop.vdop);
+        (void)fprintf(debugfp,
+                      "DOP: satellites %d, pdop=%lf, hdop=%lf, vdop=%lf\n",
+                      collect->satellites_used, collect->dop.pdop,
+                      collect->dop.hdop, collect->dop.vdop);
     if (collect->set & VERSION_SET)
-	(void)fprintf(debugfp, "VERSION: release=%s rev=%s proto=%d.%d\n",
-		      collect->version.release,
-		      collect->version.rev,
-		      collect->version.proto_major,
-		      collect->version.proto_minor);
+        (void)fprintf(debugfp, "VERSION: release=%s rev=%s proto=%d.%d\n",
+                      collect->version.release,
+                      collect->version.rev,
+                      collect->version.proto_major,
+                      collect->version.proto_minor);
     if (collect->set & POLICY_SET)
-	(void)fprintf(debugfp,
-		      "POLICY: watcher=%s nmea=%s raw=%d scaled=%s timing=%s, "
+        (void)fprintf(debugfp,
+                      "POLICY: watcher=%s nmea=%s raw=%d scaled=%s timing=%s, "
                       "split24=%s pps=%s, devpath=%s\n",
-		      collect->policy.watcher ? "true" : "false",
-		      collect->policy.nmea ? "true" : "false",
-		      collect->policy.raw,
-		      collect->policy.scaled ? "true" : "false",
-		      collect->policy.timing ? "true" : "false",
-		      collect->policy.split24 ? "true" : "false",
-		      collect->policy.pps ? "true" : "false",
-		      collect->policy.devpath);
+                      collect->policy.watcher ? "true" : "false",
+                      collect->policy.nmea ? "true" : "false",
+                      collect->policy.raw,
+                      collect->policy.scaled ? "true" : "false",
+                      collect->policy.timing ? "true" : "false",
+                      collect->policy.split24 ? "true" : "false",
+                      collect->policy.pps ? "true" : "false",
+                      collect->policy.devpath);
     if (collect->set & SATELLITE_SET) {
-	struct satellite_t *sp;
+        struct satellite_t *sp;
 
-	(void)fprintf(debugfp, "SKY: satellites in view: %d\n",
-		      collect->satellites_visible);
-	for (sp = collect->skyview;
-	     sp < collect->skyview + collect->satellites_visible;
-	     sp++) {
-	    (void)fprintf(debugfp, "  %2.2d: %4.1f %5.1f %3.0f %c\n",
-			  sp->PRN, sp->elevation,
-			  sp->azimuth, sp->ss,
-			  sp->used ? 'Y' : 'N');
-	}
+        (void)fprintf(debugfp, "SKY: satellites in view: %d\n",
+                      collect->satellites_visible);
+        for (sp = collect->skyview;
+             sp < collect->skyview + collect->satellites_visible;
+             sp++) {
+            (void)fprintf(debugfp, "  %2.2d: %4.1f %5.1f %3.0f %c\n",
+                          sp->PRN, sp->elevation,
+                          sp->azimuth, sp->ss,
+                          sp->used ? 'Y' : 'N');
+        }
     }
     if (collect->set & RAW_SET)
-	(void)fprintf(debugfp, "RAW: got raw data\n");
+        (void)fprintf(debugfp, "RAW: got raw data\n");
     if (collect->set & DEVICE_SET)
-	(void)fprintf(debugfp, "DEVICE: Device is '%s', driver is '%s'\n",
-		      collect->dev.path, collect->dev.driver);
+        (void)fprintf(debugfp, "DEVICE: Device is '%s', driver is '%s'\n",
+                      collect->dev.path, collect->dev.driver);
     if (collect->set & DEVICELIST_SET) {
-	int i;
-	(void)fprintf(debugfp, "DEVICELIST:%d devices:\n",
-		      collect->devices.ndevices);
-	for (i = 0; i < collect->devices.ndevices; i++) {
-	    (void)fprintf(debugfp, "%d: path='%s' driver='%s'\n",
-			  collect->devices.ndevices,
-			  collect->devices.list[i].path,
-			  collect->devices.list[i].driver);
-	}
+        int i;
+        (void)fprintf(debugfp, "DEVICELIST:%d devices:\n",
+                      collect->devices.ndevices);
+        for (i = 0; i < collect->devices.ndevices; i++) {
+            (void)fprintf(debugfp, "%d: path='%s' driver='%s'\n",
+                          collect->devices.ndevices,
+                          collect->devices.list[i].path,
+                          collect->devices.list[i].driver);
+        }
     }
 
 }
