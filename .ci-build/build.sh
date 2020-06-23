6af0f7d8a32602d0b55127beca4b6569cf6a62fd
Joachim Kross <kross@kaffeeschluerfer.com>
2020-06-23 08:37:38 +0200

    build.sh: Align indentation, reduce indendation width
    Proposal of four characters instead of eight, since that
    seems to be most prevalent in the remainder of the codebase.
    
    No functional changes.

--- .ci-build/build.sh
+++ .ci-build/build.sh
@@ -6,40 +6,40 @@ set -x
 
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 if uname -a | grep -qi freebsd; then
-        export PATH="${PATH}:/usr/local/bin:/usr/local/sbin"
+    export PATH="${PATH}:/usr/local/bin:/usr/local/sbin"
 fi
 
 if [ "${USE_CCACHE}" = "true" ] && [ -n "${CCACHE_DIR}" ] && command -v ccache >/dev/null; then
 
-        # create dir first, required by realpath
-        mkdir -p "${CCACHE_DIR}"
+    # create dir first, required by realpath
+    mkdir -p "${CCACHE_DIR}"
 
-        # fix for ccache: error: BASEDIR: not an absolute path
-        export CCACHE_DIR=$(realpath "${CCACHE_DIR}")
-        export CCACHE_BASEDIR=$(realpath "${CCACHE_BASEDIR}")
+    # fix for ccache: error: BASEDIR: not an absolute path
+    export CCACHE_DIR=$(realpath "${CCACHE_DIR}")
+    export CCACHE_BASEDIR=$(realpath "${CCACHE_BASEDIR}")
 
-        if [ -d "/usr/lib64/ccache" ]; then
-                # fedora
-                export PATH="/usr/lib64/ccache:${PATH}"
-        elif [ -d "/usr/local/libexec/ccache/" ]; then
-                # freebsd
-                export PATH="/usr/local/libexec/ccache:${PATH}"
-        else
-                # debian, .....
-                export PATH="/usr/lib/ccache:${PATH}"
-        fi
-        echo 'max_size = 100M' > "${CCACHE_DIR}/ccache.conf"
+    if [ -d "/usr/lib64/ccache" ]; then
+        # fedora
+        export PATH="/usr/lib64/ccache:${PATH}"
+    elif [ -d "/usr/local/libexec/ccache/" ]; then
+        # freebsd
+        export PATH="/usr/local/libexec/ccache:${PATH}"
+    else
+        # debian, .....
+        export PATH="/usr/lib/ccache:${PATH}"
+    fi
+    echo 'max_size = 100M' > "${CCACHE_DIR}/ccache.conf"
 else
-        export USE_CCACHE="false"
+    export USE_CCACHE="false"
 fi
 
 if command -v lsb_release >/dev/null && lsb_release -d | grep -qi -e debian -e ubuntu; then
-        eval "$(dpkg-buildflags --export=sh)"
-        export DEB_HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
-        export PYTHONS="$(pyversions -v -r '>= 2.3'; py3versions -v -r '>= 3.4')"
-        SCONSOPTS="${SCONSOPTS} libdir=/usr/lib/${DEB_HOST_MULTIARCH}"
+    eval "$(dpkg-buildflags --export=sh)"
+    export DEB_HOST_MULTIARCH=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
+    export PYTHONS="$(pyversions -v -r '>= 2.3'; py3versions -v -r '>= 3.4')"
+    SCONSOPTS="${SCONSOPTS} libdir=/usr/lib/${DEB_HOST_MULTIARCH}"
 else
-        SCONSOPTS="${SCONSOPTS} libdir=/usr/lib"
+    SCONSOPTS="${SCONSOPTS} libdir=/usr/lib"
 fi
 
 SCONSOPTS="${SCONSOPTS} $@ prefix=/usr"
@@ -55,17 +55,17 @@ SCONSOPTS="${SCONSOPTS} qt_versioned=5"
 export SCONS=$(command -v scons)
 
 if command -v nproc >/dev/null; then
-        NPROC=$(nproc)
-        SCONS_PARALLEL="-j ${NPROC} "
-        if [ "${SLOW_CHECK}" != "yes" ]; then
-                CHECK_NPROC=$(( 4 * ${NPROC} ))
-                SCONS_CHECK_PARALLEL="-j ${CHECK_NPROC} "
-        else
-                SCONS_CHECK_PARALLEL="${SCONS_PARALLEL}"
-        fi
+    NPROC=$(nproc)
+    SCONS_PARALLEL="-j ${NPROC} "
+    if [ "${SLOW_CHECK}" != "yes" ]; then
+        CHECK_NPROC=$(( 4 * ${NPROC} ))
+        SCONS_CHECK_PARALLEL="-j ${CHECK_NPROC} "
+    else
+        SCONS_CHECK_PARALLEL="${SCONS_PARALLEL}"
+    fi
 else
-        SCONS_PARALLEL=""
-        SCONS_CHECK_PARALLEL=""
+    SCONS_PARALLEL=""
+    SCONS_CHECK_PARALLEL=""
 fi
 
 export SCONSOPTS
@@ -84,13 +84,12 @@ for py in $PYTHONS; do
     rm -f .sconsign.*.dblite
     ${SCAN_BUILD} python${py}     ${_SCONS} ${SCONS_PARALLEL}${SCONSOPTS} build-all
     if [ -z "${NOCHECK}" ]; then
-            python${py}     ${_SCONS} ${SCONS_CHECK_PARALLEL}${SCONSOPTS} check
+        python${py}     ${_SCONS} ${SCONS_CHECK_PARALLEL}${SCONSOPTS} check
     fi
 done
 
 
 
 if [ "${USE_CCACHE}" = "true" ]; then
-        ccache -s
+    ccache -s
 fi
-
