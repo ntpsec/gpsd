6c5070b676c05fb721348d1b1d27b30358dac52e
Gary E. Miller <gem@rellim.com>
2020-08-17 19:42:36 -0700

    driver_ubx.c: Add missing skyview_time from  UBX-NAV-SVINFO

--- driver_ubx.c
+++ driver_ubx.c
@@ -1753,6 +1753,7 @@ ubx_msg_nav_svinfo(struct gps_device_t *session, unsigned char *buf,
                    size_t data_len)
 {
     unsigned int i, nchan, nsv, st;
+    timespec_t ts_tow;
 
     if (8 > data_len) {
         GPSD_LOG(LOG_PROG, &session->context->errout,
@@ -1761,6 +1762,10 @@ ubx_msg_nav_svinfo(struct gps_device_t *session, unsigned char *buf,
     }
 
     session->driver.ubx.iTOW = getles32(buf, 0);
+    MSTOTS(&ts_tow, session->driver.ubx.iTOW);
+    session->gpsdata.skyview_time =
+        gpsd_gpstime_resolv(session, session->context->gps_week, ts_tow);
+
     nchan = (unsigned int)getub(buf, 4);
     if (nchan > MAXCHANNELS) {
         GPSD_LOG(LOG_WARN, &session->context->errout,
@@ -1825,8 +1830,6 @@ ubx_msg_nav_svinfo(struct gps_device_t *session, unsigned char *buf,
     /* UBX does not give us these, so recompute */
     session->gpsdata.dop.xdop = NAN;
     session->gpsdata.dop.ydop = NAN;
-    session->gpsdata.skyview_time.tv_sec = 0;
-    session->gpsdata.skyview_time.tv_nsec = 0;
     session->gpsdata.satellites_visible = (int)st;
     session->gpsdata.satellites_used = (int)nsv;
     GPSD_LOG(LOG_DATA, &session->context->errout,
