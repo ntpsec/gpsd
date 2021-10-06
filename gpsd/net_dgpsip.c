5d5912b05a3e44d68eb99d99b06ccc23bc610092
Gary E. Miller <gem@rellim.com>
2021-10-06 13:00:14 -0700

    gpsd/net_dgpsip.c: Better error LOG for netsock_sonnect()
    Some Yoday styling, // comments, add braces

--- gpsd/net_dgpsip.c
+++ gpsd/net_dgpsip.c
@@ -4,8 +4,9 @@
  * SPDX-License-Identifier: BSD-2-clause
  */
 
-#include "../include/gpsd_config.h"  /* must be before all includes */
+#include "../include/gpsd_config.h"   // must be before all includes
 
+#include <errno.h>                    // for errno
 #include <fcntl.h>
 #include <netdb.h>
 #include <stdbool.h>
@@ -18,45 +19,58 @@
 
 #include "../include/gpsd.h"
 
+/* open a connection to a DGPSIP server
+ * Return: socket on success
+ *         less than zero on failure
+ */
 socket_t dgpsip_open(struct gps_device_t *device, const char *dgpsserver)
-/* open a connection to a DGPSIP server */
 {
     char *colon, *dgpsport = "rtcm-sc104";
     int opts;
+    char hn[256], buf[BUFSIZ];
+    socket_t dsock;
 
+    device->servicetype = SERVICE_DGPSIP;
     device->dgpsip.reported = false;
-    if ((colon = strchr(dgpsserver, ':')) != NULL) {
+    if (NULL != (colon = strchr(dgpsserver, ':'))) {
         dgpsport = colon + 1;
         *colon = '\0';
     }
-    if (!getservbyname(dgpsport, "tcp"))
+    if (!getservbyname(dgpsport, "tcp")) {
         dgpsport = DEFAULT_RTCM_PORT;
+    }
 
-    device->gpsdata.gps_fd =
-        netlib_connectsock(AF_UNSPEC, dgpsserver, dgpsport, "tcp");
-    if (device->gpsdata.gps_fd >= 0) {
-        char hn[256], buf[BUFSIZ];
-        GPSD_LOG(LOG_PROG, &device->context->errout,
-                 "connection to DGPS server %s established.\n",
-                 dgpsserver);
-        (void)gethostname(hn, sizeof(hn));
-        /* greeting required by some RTCM104 servers; others will ignore it */
-        (void)snprintf(buf, sizeof(buf), "HELO %s gpsd %s\r\nR\r\n", hn,
-                       VERSION);
-        if (write(device->gpsdata.gps_fd, buf, strlen(buf)) !=
-            (ssize_t) strlen(buf))
-            GPSD_LOG(LOG_ERROR, &device->context->errout,
-                     "hello to DGPS server %s failed\n",
-                     dgpsserver);
-    } else
+    dsock = netlib_connectsock(AF_UNSPEC, dgpsserver, dgpsport, "tcp");
+    if (0 <= dsock) {
         GPSD_LOG(LOG_ERROR, &device->context->errout,
-                 "can't connect to DGPS server %s, netlib error %d.\n",
-                 dgpsserver, device->gpsdata.gps_fd);
+                 "DGPS: can't connect to DGPS server %s, netlib error %s(%d).\n",
+                 dgpsserver, netlib_errstr(dsock), dsock);
+        device->gpsdata.gps_fd = PLACEHOLDING_FD;
+        return dsock;
+    }
+    GPSD_LOG(LOG_PROG, &device->context->errout,
+             "DGPS: connection to DGPS server %s established. fd=%d\n",
+             dgpsserver, dsock);
+    device->gpsdata.gps_fd = dsock;
+    (void)gethostname(hn, sizeof(hn));
+    // greeting required by some RTCM104 servers; others will ignore it
+    (void)snprintf(buf, sizeof(buf), "HELO %s gpsd %s\r\nR\r\n", hn,
+                   VERSION);
+    if ((ssize_t)strlen(buf) !=
+        write(device->gpsdata.gps_fd, buf, strlen(buf))) {
+        GPSD_LOG(LOG_ERROR, &device->context->errout,
+                 "DGPS: hello to DGPS server %s failed\n",
+                 dgpsserver);
+    }
     opts = fcntl(device->gpsdata.gps_fd, F_GETFL);
 
-    if (opts >= 0)
+    if (0 <= opts) {
         (void)fcntl(device->gpsdata.gps_fd, F_SETFL, opts | O_NONBLOCK);
-    device->servicetype = SERVICE_DGPSIP;
+    } else {
+        GPSD_LOG(LOG_ERROR, &device->context->errout,
+                 "DGPS: fcntl %s failed. %s(%d)\n",
+                 dgpsserver, strerror(errno), errno);
+    }
     return device->gpsdata.gps_fd;
 }
 
@@ -79,9 +93,9 @@ void dgpsip_report(struct gps_context_t *context,
                            gps->gpsdata.fix.altMSL);
             if (write(dgpsip->gpsdata.gps_fd, buf, strlen(buf)) ==
                 (ssize_t) strlen(buf))
-                GPSD_LOG(LOG_IO, &context->errout, "=> dgps %s\n", buf);
+                GPSD_LOG(LOG_IO, &context->errout, "DGPS: => dgps %s\n", buf);
             else
-                GPSD_LOG(LOG_IO, &context->errout, "write to dgps FAILED\n");
+                GPSD_LOG(LOG_IO, &context->errout, "DGPS: write to dgps FAILED\n");
         }
     }
 }
