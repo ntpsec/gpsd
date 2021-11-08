4d649e53097df9fceb1d2066315a735f971299a0
Gary E. Miller <gem@rellim.com>
2021-11-08 14:10:47 -0800

    gpsd/shmexport.c: Prevent lingering shmid.  Style cleanup.
    Tweak log messages. // comments.

--- gpsd/shmexport.c
+++ gpsd/shmexport.c
@@ -28,9 +28,13 @@ PERMISSIONS
 #include <sys/time.h>
 
 #include "../include/gpsd.h"
-#include "../include/libgps.h" /* for SHM_PSEUDO_FD */
+#include "../include/libgps.h"    // for SHM_PSEUDO_FD
 
-/* initialize the shared-memory segment to be used for export */
+/* initialize the shared-memory segment to be used for export
+ *
+ * Return: true = OK
+ *         false: failed
+ */
 bool shm_acquire(struct gps_context_t *context)
 {
     long shmkey = getenv("GPSD_SHM_KEY") ? \
@@ -38,57 +42,63 @@ bool shm_acquire(struct gps_context_t *context)
 
     int shmid = shmget((key_t)shmkey, sizeof(struct shmexport_t),
                        (int)(IPC_CREAT|0666));
+    context->shmid = shmid;
     if (-1 == shmid) {
         GPSD_LOG(LOG_ERROR, &context->errout,
-                 "shmget(0x%lx, %zd, 0666) for SHM export failed: %s\n",
+                 "SHM: shmget(0x%lx, %zd, 0666) SHM export failed: %s(%d)\n",
                  shmkey,
                  sizeof(struct shmexport_t),
-                 strerror(errno));
+                 strerror(errno), errno);
         return false;
     }
 
     GPSD_LOG(LOG_PROG, &context->errout,
-             "shmget(0x%lx, %zd, 0666) for SHM export succeeded\n",
+             "SHM: shmget(0x%lx, %zd, 0666) for SHM export succeeded\n",
              shmkey,
              sizeof(struct shmexport_t));
 
     context->shmexport = (void *)shmat(shmid, 0, 0);
-    if ((void *) -1 == context->shmexport) {
+    if ((void *)-1 == context->shmexport) {
         GPSD_LOG(LOG_ERROR, &context->errout,
-                 "shmat failed: %s\n", strerror(errno));
+                 "SHM: shmat failed: %s(%d)\n", strerror(errno), errno);
         context->shmexport = NULL;
+        // close shmid
+        shm_release(context);
         return false;
     }
-    context->shmid = shmid;
 
     GPSD_LOG(LOG_PROG, &context->errout,
-             "shmat() for SHM export succeeded, segment %d\n", shmid);
+             "SHM: shmat() for SHM export succeeded, segment %d\n", shmid);
     return true;
 }
 
 // release the shared-memory segment used for export
 void shm_release(struct gps_context_t *context)
 {
-    if (NULL == context->shmexport)
-        return;
 
     /* Mark shmid to go away when no longer used
      * Having it linger forever is bad, and when the size enlarges
      * it can no longer be opened
      */
-    if (-1 == shmctl(context->shmid, IPC_RMID, NULL)) {
-        GPSD_LOG(LOG_WARN, &context->errout,
-                 "shmctl(%d) for IPC_RMID failed, %s(%d)\n",
-                 context->shmid, strerror(errno), errno);
+    if (-1 != context->shmid) {
+        if (-1 == shmctl(context->shmid, IPC_RMID, NULL)) {
+            GPSD_LOG(LOG_WARN, &context->errout,
+                     "SHM: shmctl(%d) for IPC_RMID failed, %s(%d)\n",
+                     context->shmid, strerror(errno), errno);
+        }
     }
-    if (-1 == shmdt((const void *)context->shmexport)) {
-        GPSD_LOG(LOG_WARN, &context->errout,
-                 "shmdt) for id %d, %s(%d)\n",
-                 context->shmid, strerror(errno), errno);
+    context->shmid = -1;
+
+    if (NULL != context->shmexport) {
+        if (-1 == shmdt((const void *)context->shmexport)) {
+            GPSD_LOG(LOG_WARN, &context->errout,
+                     "SHM: shmdt() for shmid %d failed: %s(%d)\n",
+                     context->shmid, strerror(errno), errno);
+        }
     }
 }
 
-/* export an update to all listeners */
+// export an update to all listeners
 void shm_update(struct gps_context_t *context, struct gps_data_t *gpsdata)
 {
     if (NULL != context->shmexport) {
@@ -120,14 +130,13 @@ void shm_update(struct gps_context_t *context, struct gps_data_t *gpsdata)
         shared->gpsdata.gps_fd = SHM_PSEUDO_FD;
 #else
         shared->gpsdata.gps_fd = (void *)(intptr_t)SHM_PSEUDO_FD;
-#endif /* USE_QT */
+#endif  // USE_QT
         memory_barrier();
         shared->bookend1 = tick;
     }
 }
 
 
-#endif /* SHM_EXPORT_ENABLE */
+#endif  // SHM_EXPORT_ENABLE
 
-/* end */
 // vim: set expandtab shiftwidth=4
