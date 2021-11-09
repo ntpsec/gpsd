026bc6e4d2b2ea777074335814fda582781483b0
Gary E. Miller <gem@rellim.com>
2021-11-08 18:40:59 -0800

    gpsd/dbusexport.c: Style cleanups.  No functional changes
    Yoda style.  // comments, etc.

--- gpsd/dbusexport.c
+++ gpsd/dbusexport.c
@@ -7,18 +7,20 @@
  *
  * See "man 8 gpsd" for description of the gpsd D-Bus message.
  */
-#include "../include/gpsd_config.h"  /* must be before all includes */
+#include "../include/gpsd_config.h"    // must be before all includes
 
-#include "../include/gpsd.h"
 #if defined(DBUS_EXPORT_ENABLE)
 #include <dbus/dbus.h>
+#include "../include/gpsd.h"
 
 static DBusConnection *connection = NULL;
 
 /*
  * Does what is required to initialize the dbus connection
  * This is pretty basic at this point, as we don't receive commands via dbus.
- * Returns 0 when everything is OK.
+ *
+ * Return: 0 OK.
+ *         1 fail
  */
 int initialize_dbus_connection(void)
 {
@@ -26,36 +28,38 @@ int initialize_dbus_connection(void)
 
     dbus_error_init(&error);
     connection = dbus_bus_get(DBUS_BUS_SYSTEM, &error);
-    if (connection == NULL) {
+    if (NULL == connection) {
         dbus_error_free(&error);
-        /* report error */
+        // report error
         return 1;
     }
     return 0;
 }
 
-/* sends the current fix data for this channel via dbus */
+// sends the current fix data for this channel via dbus
 void send_dbus_fix(struct gps_device_t *channel)
 {
     struct gps_data_t *gpsdata;
     struct gps_fix_t *gpsfix;
     DBusMessage *message;
-    /*DBusMessageIter   iter; */
-    dbus_uint32_t serial;       /* collected, but not used */
+    // DBusMessageIter   iter;
+    dbus_uint32_t serial;       // collected, but not used
     char *gpsd_devname;
     double dtime;               // time as a double, loss of precision!
 
-    /* if the connection is non existent, return without doing anything */
-    if (connection == NULL)
+    // if the connection is non existent, return without doing anything
+    if (NULL == connection) {
         return;
+    }
 
     gpsdata = &(channel->gpsdata);
     gpsfix = &(gpsdata->fix);
     gpsd_devname = gpsdata->dev.path;
 
-    /* Send the named signal.  */
+    // Send the named signal.
     /* the dbus/locationd doc fails to specify altitude as WGS84 or MSL.
-     * assume altMSL */
+     * assume altMSL
+     */
     message = dbus_message_new_signal("/org/gpsd", "org.gpsd", "fix");
     dtime = TSTONS(&gpsfix->time);
     dbus_message_append_args(message,
@@ -80,5 +84,5 @@ void send_dbus_fix(struct gps_device_t *channel)
     dbus_message_unref(message);
 }
 
-#endif /* defined(DBUS_EXPORT_ENABLE) */
+#endif  // DBUS_EXPORT_ENABLE
 // vim: set expandtab shiftwidth=4
