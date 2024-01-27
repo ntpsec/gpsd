582b559856d37f6cc67f3b2ee83c1e61198b5f88
Gary E. Miller <gem@rellim.com>
2024-01-26 18:40:04 -0800

    tests/test_libgps.c: Style updates.
    // comments.  Yoda style.  Always braces.

--- tests/test_libgps.c
+++ tests/test_libgps.c
@@ -3,7 +3,7 @@
  * Not really useful for anything but debugging.
  * SPDX-License-Identifier: BSD-2-clause
  */
-#include "../include/gpsd_config.h"  /* must be before all includes */
+#include "../include/gpsd_config.h"  // must be before all includes
 
 #include <ctype.h>
 #include <errno.h>
@@ -27,7 +27,8 @@ static void onsig(int sig)
 }
 
 #ifdef SOCKET_EXPORT_ENABLE
-/* must start zeroed, otherwise the unit test will try to chase garbage pointer fields. */
+/* Must start zeroed, otherwise the unit test will try to chase garbage
+ * pointer fields. */
 static struct gps_data_t gpsdata;
 #endif
 
@@ -42,12 +43,13 @@ int main(int argc, char *argv[])
     char *fmsg = NULL;
     int debug = 0;
 
+    // FIXME: signal() not portable.  Use sigaction() instead.
     (void)signal(SIGSEGV, onsig);
 #ifdef SIGBUS
     (void)signal(SIGBUS, onsig);
 #endif
 
-    while ((option = getopt(argc, argv, "bf:hsD:?")) != -1) {
+    while (-1 != (option = getopt(argc, argv, "bf:hsD:?"))) {
 	switch (option) {
 	case 'b':
 	    batchmode = true;
@@ -57,16 +59,15 @@ int main(int argc, char *argv[])
 	    fmsg = optarg;
 	    break;
 	case 's':
-	    (void)
-		printf("Sizes: fix=%zu gpsdata=%zu rtcm2=%zu rtcm3=%zu "
-                       "ais=%zu compass=%zu raw=%zu devices=%zu policy=%zu "
-                       "version=%zu, noise=%zu\n",
-		 sizeof(struct gps_fix_t),
-		 sizeof(struct gps_data_t), sizeof(struct rtcm2_t),
-		 sizeof(struct rtcm3_t), sizeof(struct ais_t),
-		 sizeof(struct attitude_t), sizeof(struct rawdata_t),
-		 sizeof(collect.devices), sizeof(struct gps_policy_t),
-		 sizeof(struct version_t), sizeof(struct gst_t));
+	    (void)printf("Sizes: fix=%zu gpsdata=%zu rtcm2=%zu rtcm3=%zu "
+                         "ais=%zu compass=%zu raw=%zu devices=%zu policy=%zu "
+                         "version=%zu, noise=%zu\n",
+		         sizeof(struct gps_fix_t),
+		         sizeof(struct gps_data_t), sizeof(struct rtcm2_t),
+		         sizeof(struct rtcm3_t), sizeof(struct ais_t),
+		         sizeof(struct attitude_t), sizeof(struct rawdata_t),
+		         sizeof(collect.devices), sizeof(struct gps_policy_t),
+		         sizeof(struct version_t), sizeof(struct gst_t));
 	    exit(EXIT_SUCCESS);
 	case 'D':
 	    debug = atoi(optarg);
@@ -74,39 +75,42 @@ int main(int argc, char *argv[])
 	case '?':
 	case 'h':
 	default:
-	    (void)fputs("usage: test_libgps [-b] [-f fwdmsg] [-D lvl] [-s] [server[:port:[device]]]\n", stderr);
+	    (void)fputs("usage: test_libgps [-b] [-f fwdmsg] [-D lvl] "
+                        "[-s] [server[:port:[device]]]\n", stderr);
 	    exit(EXIT_FAILURE);
 	}
     }
 
-    /* Grok the server, port, and device. */
+    // Grok the server, port, and device.
     if (optind < argc) {
 	gpsd_source_spec(argv[optind], &source);
-    } else
+    } else {
 	gpsd_source_spec(NULL, &source);
+    }
 
     gps_enable_debug(debug, stdout);
     if (batchmode) {
 #ifdef SOCKET_EXPORT_ENABLE
-	while (fgets(buf, sizeof(buf), stdin) != NULL) {
-	    if (buf[0] == '{' || isalpha( (int) buf[0])) {
+	while (NULL != fgets(buf, sizeof(buf), stdin)) {
+	    if ('{'  == buf[0] ||
+                isalpha( (int) buf[0])) {
 		gps_unpack(buf, &gpsdata);
 		libgps_dump_state(&gpsdata);
 	    }
 	}
 #endif
-    } else if (gps_open(source.server, source.port, &collect) != 0) {
+    } else if (0 != gps_open(source.server, source.port, &collect)) {
 	(void)fprintf(stderr,
 		      "test_libgps: no gpsd running or network error: %d, %s\n",
 		      errno, gps_errstr(errno));
 	exit(EXIT_FAILURE);
     } else if (forwardmode) {
-	if (gps_send(&collect, fmsg) == -1) {
+	if (-1 == gps_send(&collect, fmsg)) {
 	  (void)fprintf(stderr,
 			"test_libgps: gps send error: %d, %s\n",
 			errno, gps_errstr(errno));
 	}
-	if (gps_read(&collect, NULL, 0) == -1) {
+	if (-1 == gps_read(&collect, NULL, 0)) {
 	  (void)fprintf(stderr,
 			"test_libgps: gps read error: %d, %s\n",
 			errno, gps_errstr(errno));
@@ -118,14 +122,17 @@ int main(int argc, char *argv[])
     } else {
 	int tty = isatty(0);
 
-	if (tty)
+	if (tty) {
 	    (void)fputs("This is the gpsd exerciser.\n", stdout);
+        }
 	for (;;) {
-	    if (tty)
+	    if (tty) {
 		(void)fputs("> ", stdout);
-	    if (fgets(buf, sizeof(buf), stdin) == NULL) {
-		if (tty)
+            }
+	    if (NULL == fgets(buf, sizeof(buf), stdin)) {
+		if (tty) {
 		    putchar('\n');
+                }
 		break;
 	    }
 	    collect.set = 0;
