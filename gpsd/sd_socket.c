5242d2c10f983a524ffcbb5d9950a480053d8a0a
Gary E. Miller <gem@rellim.com>
2024-01-31 13:41:12 -0800

    gpsd/sd_socket.c: Style cleanups.  Better error checking.
    // comments.  add braces.  Always check errno fro strtoul().

--- gpsd/sd_socket.c
+++ gpsd/sd_socket.c
@@ -1,13 +1,14 @@
 /*
  * This file is Copyright 2011 by Eckhart Wörner
- * This file is Copyright 2011 by the GPSD project
+ * This file is Copyright by the GPSD project
  * SPDX-License-Identifier: BSD-2-clause
  */
 
-#include "../include/gpsd_config.h"  /* must be before all includes */
+#include "../include/gpsd_config.h"  // must be before all includes
 
+#include <errno.h>                   // for errno
 #include <limits.h>
-#include <stdlib.h>
+#include <stdlib.h>                  // for strtoul()
 #include <unistd.h>
 
 #include "../include/sd_socket.h"
@@ -17,21 +18,29 @@ int sd_get_socket_count(void) {
     const char* env;
 
     env = getenv("LISTEN_PID");
-    if (!env)
+    if (!env) {
         return 0;
+    }
 
+    errno = 0;
     n = strtoul(env, NULL, 10);
-    if (n == ULONG_MAX || (pid_t)n != getpid())
+    if (0 != errno ||
+        getpid() != (pid_t)n) {
         return 0;
+    }
 
     env = getenv("LISTEN_FDS");
-    if (!env)
+    if (!env) {
         return 0;
+    }
 
+    errno = 0;
     n = strtoul(env, NULL, 10);
-    if (n == ULONG_MAX)
+    if (0 != errno) {
         return 0;
+    }
 
     return (int)n;
 }
+
 // vim: set expandtab shiftwidth=4
