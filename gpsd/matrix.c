a45c2ef0d5bb8743438ee7c1699b723ede865503
Gary E. Miller <gem@rellim.com>
2024-01-31 16:34:01 -0800

    gpsd/matrix.c: Style tweaks.  // comments, operator placement.

--- gpsd/matrix.c
+++ gpsd/matrix.c
@@ -1,17 +1,17 @@
 /* matrix.c - matrix-algebra code
  *
- * This file is Copyright 2014 by the GPSD project
+ * This file is Copyright by the GPSD project
  * SPDX-License-Identifier: BSD-2-clause
  */
 
-#include "../include/gpsd_config.h"  /* must be before all includes */
+#include "../include/gpsd_config.h"  // must be before all includes
 
 #include <math.h>
 #include <stdbool.h>
 
 #include "../include/matrix.h"
 
-/* selected elements from 4x4 matrox inversion */
+// selected elements from 4x4 matrox inversion
 bool matrix_invert(double mat[4][4], double inverse[4][4])
 {
     // Find all NECESSARY 2x2 subdeterminants
@@ -35,8 +35,8 @@ bool matrix_invert(double mat[4][4], double inverse[4][4])
     double Det2_23_23 = mat[2][2] * mat[3][3] - mat[2][3] * mat[3][2];
 
     // Find all NECESSARY 3x3 subdeterminants
-    double Det3_012_012 = mat[0][0] * Det2_12_12 - mat[0][1] * Det2_12_02
-        + mat[0][2] * Det2_12_01;
+    double Det3_012_012 = mat[0][0] * Det2_12_12 - mat[0][1] * Det2_12_02 +
+        mat[0][2] * Det2_12_01;
     //double Det3_012_013 = mat[0][0]*Det2_12_13 - mat[0][1]*Det2_12_03
     //                            + mat[0][3]*Det2_12_01;
     //double Det3_012_023 = mat[0][0]*Det2_12_23 - mat[0][2]*Det2_12_03
@@ -45,8 +45,8 @@ bool matrix_invert(double mat[4][4], double inverse[4][4])
     //                            + mat[0][3]*Det2_12_12;
     //double Det3_013_012 = mat[0][0]*Det2_13_12 - mat[0][1]*Det2_13_02
     //                            + mat[0][2]*Det2_13_01;
-    double Det3_013_013 = mat[0][0] * Det2_13_13 - mat[0][1] * Det2_13_03
-        + mat[0][3] * Det2_13_01;
+    double Det3_013_013 = mat[0][0] * Det2_13_13 - mat[0][1] * Det2_13_03 +
+        mat[0][3] * Det2_13_01;
     //double Det3_013_023 = mat[0][0]*Det2_13_23 - mat[0][2]*Det2_13_03
     //                            + mat[0][3]*Det2_13_02;
     //double Det3_013_123 = mat[0][1]*Det2_13_23 - mat[0][2]*Det2_13_13
@@ -55,28 +55,30 @@ bool matrix_invert(double mat[4][4], double inverse[4][4])
     //                            + mat[0][2]*Det2_23_01;
     //double Det3_023_013 = mat[0][0]*Det2_23_13 - mat[0][1]*Det2_23_03
     //                            + mat[0][3]*Det2_23_01;
-    double Det3_023_023 = mat[0][0] * Det2_23_23 - mat[0][2] * Det2_23_03
-        + mat[0][3] * Det2_23_02;
+    double Det3_023_023 = mat[0][0] * Det2_23_23 - mat[0][2] * Det2_23_03 +
+        mat[0][3] * Det2_23_02;
     //double Det3_023_123 = mat[0][1]*Det2_23_23 - mat[0][2]*Det2_23_13
     //                            + mat[0][3]*Det2_23_12;
-    double Det3_123_012 = mat[1][0] * Det2_23_12 - mat[1][1] * Det2_23_02
-        + mat[1][2] * Det2_23_01;
-    double Det3_123_013 = mat[1][0] * Det2_23_13 - mat[1][1] * Det2_23_03
-        + mat[1][3] * Det2_23_01;
-    double Det3_123_023 = mat[1][0] * Det2_23_23 - mat[1][2] * Det2_23_03
-        + mat[1][3] * Det2_23_02;
-    double Det3_123_123 = mat[1][1] * Det2_23_23 - mat[1][2] * Det2_23_13
-        + mat[1][3] * Det2_23_12;
+    double Det3_123_012 = mat[1][0] * Det2_23_12 - mat[1][1] * Det2_23_02 +
+        mat[1][2] * Det2_23_01;
+    double Det3_123_013 = mat[1][0] * Det2_23_13 - mat[1][1] * Det2_23_03 +
+        mat[1][3] * Det2_23_01;
+    double Det3_123_023 = mat[1][0] * Det2_23_23 - mat[1][2] * Det2_23_03 +
+        mat[1][3] * Det2_23_02;
+    double Det3_123_123 = mat[1][1] * Det2_23_23 - mat[1][2] * Det2_23_13 +
+        mat[1][3] * Det2_23_12;
 
     // Find the 4x4 determinant
     static double det;
-    det = mat[0][0] * Det3_123_123
-        - mat[0][1] * Det3_123_023
-        + mat[0][2] * Det3_123_013 - mat[0][3] * Det3_123_012;
+    det = mat[0][0] * Det3_123_123 -
+        mat[0][1] * Det3_123_023 +
+        mat[0][2] * Det3_123_013 -
+        mat[0][3] * Det3_123_012;
 
     // Very small determinants probably reflect floating-point fuzz near zero
-    if (fabs(det) < 0.0001)
+    if (0.0001 > fabs(det)) {
         return false;
+    }
 
     inverse[0][0] = Det3_123_123 / det;
     //inverse[0][1] = -Det3_023_123 / det;
@@ -103,12 +105,13 @@ bool matrix_invert(double mat[4][4], double inverse[4][4])
 
 #ifdef __UNUSED_
 // cppcheck-suppress unusedFunction
+
+// symmetrize a matrix, multiply it by its transpose
 void matrix_symmetrize(double mat[4][4], double prod[4][4])
-/* symmetrize a matrix, multiply it by its transpose */
 {
     int i, j, k;
 
-    for (i = 0; i < 4; ++i) {   //< rows
+    for (i = 0; i < 4; ++i) {           //< rows
         for (j = 0; j < 4; ++j) {       //< cols
             prod[i][j] = 0.0;
             for (k = 0; k < 4; ++k) {
@@ -117,7 +120,6 @@ void matrix_symmetrize(double mat[4][4], double prod[4][4])
         }
     }
 }
-#endif /* UNUSED */
+#endif  // UNUSED
 
-/* end */
 // vim: set expandtab shiftwidth=4
