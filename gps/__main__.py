c7c434eef468f10e90fbd80df8868822a46ad5b8
Gary E. Miller <gem@rellim.com>
2024-02-15 13:16:57 -0800

    gps/__main__.py: Add a main() to the gps module.
    All it does is print the version.  Good enough to smoke out
    old gps versions.

---
+++ gps/__main__.py
@@ -0,0 +1,11 @@
+#
+# This file is Copyright by the GPSD project
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+import gps
+
+if __name__ == "__main__":
+    print("gps module version: %s" % gps.__version__)
+
+
