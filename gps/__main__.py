fa22a92ae91b972000c9638977ada9dc0cedc516
Gary E. Miller <gem@rellim.com>
2024-12-05 16:28:57 -0800

    gps/__main__.py: Fix two Codacy nits: Missing docstring, extra \n

--- gps/__main__.py
+++ gps/__main__.py
@@ -2,10 +2,10 @@
 # This file is Copyright by the GPSD project
 # SPDX-License-Identifier: BSD-2-Clause
 #
+"""Module to interface with gpsd."""
 
 import gps
 
 if __name__ == "__main__":
+    """Convenient way to get module version"""
     print("gps module version: %s" % gps.__version__)
-
-
