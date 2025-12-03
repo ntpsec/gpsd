e22d2593a0937321bcb778d6ff54310784ae7945
Gary E. Miller <gem@rellim.com>
2025-12-02 19:51:36 -0800

    android/gpsd_wrapper.c: Point out many bad problems in this file.

--- android/gpsd_wrapper.c
+++ android/gpsd_wrapper.c
@@ -1,3 +1,8 @@
+/*
+ * This file is Copyright by the GPSD project
+ * SPDX-License-Identifier: BSD-2-clause
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <android/log.h>
@@ -8,17 +13,26 @@ int main (){
 	char gpsd_params[PROP_VALUE_MAX];
 	char cmd[1024];
 	int i = 0;
-	property_get("service.gpsd.parameters", gpsd_params, "-Nn,-D2,/dev/ttyACM0,/dev/ttyACM1");
-	while (gpsd_params[i] != 0){
-		if (gpsd_params[i] == ',') gpsd_params[i] = ' ';
+
+	property_get("service.gpsd.parameters", gpsd_params,
+                     "-Nn,-D2,/dev/ttyACM0,/dev/ttyACM1");
+	while (0 != gpsd_params[i]){
+                // FIXME: gpsd_params are not checked for command injection
+		if (gpsd_params[i] == ',') {
+                    gpsd_params[i] = ' ';
+                }
 		i++;
 	}
 
+        // FIXME: use snprintf() to prevent overflow of cmd
 	sprintf(cmd, "/vendor/bin/logwrapper /vendor/bin/gpsd %s", gpsd_params);
 
-	__android_log_print(ANDROID_LOG_DEBUG, "gpsd_wrapper", "Starting gpsd: %s", cmd);
+	__android_log_print(ANDROID_LOG_DEBUG, "gpsd_wrapper",
+                            "Starting gpsd: %s", cmd);
 
+        // FIXME: gpsd_params are not checked for command injection
 	system(cmd);
 	return 0;
 }
 
+// vim: set expandtab shiftwidth=4
